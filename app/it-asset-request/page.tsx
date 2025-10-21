b195ba566e0f3c552465616d0bc8d82636e40f59
aliriocerafica <ceraficaalejandrojr@gmail.com>
2025-10-21 23:17:49 +0800

    style: Remove page header from IT Asset Request page

--- app/it-asset-request/page.tsx
+++ app/it-asset-request/page.tsx
@@ -27,12 +27,6 @@ export default function ITAssetRequestPage() {
       <Topbar />
       
       <div className="max-w-4xl mx-auto px-4 py-12">
-        {/* Page Header */}
-        <div className="mb-8 text-center">
-          <h1 className="text-4xl font-bold text-gray-900 mb-2">IT Asset Request</h1>
-          <p className="text-gray-600">Request IT equipment and resources needed for your work</p>
-        </div>
-
         {/* Form */}
         <ITAssetRequestForm />
 
